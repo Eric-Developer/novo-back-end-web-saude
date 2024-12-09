ff1cbb0e7169347d45f407c4d7982dd0dad65977
eric-developer <ericspsantos@gmail.com>
2024-12-09 10:44:01 +0100

    inicializando o projeto

---
+++ src/database/config.ts
@@ -0,0 +1,20 @@
+import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.PG_HOST,
+  port: parseInt(process.env.PG_PORT || '5432'),
+  username: process.env.PG_USUARIO,
+  password: process.env.PG_SENHA,
+  database: process.env.PG_BANCO, 
+  synchronize: true, //alterar para true para gerar a entidade
+  logging: true,
+  entities: [`${__dirname}/../models/*.{js,ts}`],
+  migrations: [
+  ],
+  subscribers: [],
+});
+ 
+export default AppDataSource;
